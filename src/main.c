061fe0f79ad315da37ca28fae7f2cf24d371e04a
Lutecea <oceane.piatpro@gmail.com>
2024-10-28 10:56:18 +0100

    Vraie mise à jour (j'étais dans le mauvais fichier...)

--- src/main.c
+++ src/main.c
@@ -4,11 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <stdio.h>
-
-int main(void)
-{
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
-
-	return 0;
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#define LED_YELLOW_NODE DT_ALIAS(led_yellow)
+const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE, gpios, {0});
+int main(void) {
+gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_HIGH);
 }
