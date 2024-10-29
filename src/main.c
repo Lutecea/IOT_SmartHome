8f4a185159507c7a540020451d6440c5f690fdee
Lutecea <oceane.piatpro@gmail.com>
2024-10-29 10:58:51 +0100

    L'écran LCD fonctionne, LCD_init fonctionnel, LCD. c et .h fonctionnels (il manque encore le write_clear qu'il faut écrire)

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include "../driver-lcd1602-i2c/lcd_screen_i2c.h"
+#include <zephyr/drivers/i2c.h>
+#include <stdio.h>
 
 #define LED_YELLOW_NODE DT_ALIAS(led_yellow)
+#define LCD_SCREEN_NODE DT_ALIAS(lcd_screen)
 
 const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE, gpios, {0});
+const struct i2c_dt_spec lcd_screen_dev = I2C_DT_SPEC_GET(LCD_SCREEN_NODE);
 
 int main(void) 
 {
@@ -12,11 +16,11 @@ int main(void)
 
 
     // Init LCD device
-    init_lcd(lcd-screen);
+    init_lcd(&lcd_screen_dev);
 
     // Display a message
-    write_lcd(lcd-screen, HELLO_MSG, LCD_LINE_1);
-    //write_lcd_clear(&dev_lcd_screen, ZEPHYR_MSG LCD_LINE_2);
+    //write_lcd(&lcd_screen_dev, ZEPHYR_MSG, LCD_LINE_1);
+    //write_lcd_clear(&lcd_screen_dev, ZEPHYR_MSG LCD_LINE_2);
 }
 
 
